bd99a35b872e8b8e1e0e5b9ec9aeca4706bb6790
Saymon Felipe <linnubr@gmail.com>
2024-07-27 17:51:55 -0300

    Limpeza do input de tag ou funcao no submit

--- src/components/registerTagOrFunction.vue
+++ src/components/registerTagOrFunction.vue
@@ -40,6 +40,7 @@ export default {
             }
 
             api.post("/igreja/" + path, data).then(function () {
+                $("#entity").val("");
                 self.$emit("success", true);
             })
         }
